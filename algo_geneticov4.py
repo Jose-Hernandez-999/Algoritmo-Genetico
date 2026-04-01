3e5ffac70411d834abf986c5968f0dcde012bc11
Jose E. Hernandez Calle <jose.hernandez@gcm.local>
2026-04-01 16:58:17 -0500

    __repr__ terminado

--- algo_geneticov4.py
+++ algo_geneticov4.py
@@ -40,6 +40,7 @@ print()
 print(f'genoma: {individuo}')
 print()
 
+
 #falta hacer que me devuelva un diccionario, terminar de definir clase individuo y agregar metodo para
 # calcular fitness en clase Individuo
 #agregar funcion que penalice layouts
